4cce25a3bab85e6c4959b18269ee8ee6cb2d01f7
LudovDI <dima-l-01@mail.ru>
2022-05-03 21:46:22 +0300

    Урок 7

--- src/lesson7/JavaDynamicTasks.java
+++ src/lesson7/JavaDynamicTasks.java
@@ -1,8 +1,9 @@
 package lesson7;
 
 import kotlin.NotImplementedError;
+import kotlin.collections.EmptyList;
 
-import java.util.List;
+import java.util.*;
 
 @SuppressWarnings("unused")
 public class JavaDynamicTasks {
@@ -17,11 +18,45 @@ public class JavaDynamicTasks {
      * Если общей подпоследовательности нет, вернуть пустую строку.
      * Если есть несколько самых длинных общих подпоследовательностей, вернуть любую из них.
      * При сравнении подстрок, регистр символов *имеет* значение.
+     *
+     * Трудоемкость T = O((first.length()+1)*(second.length()+1)) - вложенный цикл из 2 уровней,
+     *      один цикл с first.length()+1 операцией, второй цикл с second.length()+1 операцией и
+     *      цикл while с max(second.length, first.length) операцией
+     * Ресурсоемкость R = O((first.length()+1)*(second.length()+1)) -
+     *      двумерный массив размером (first.length()+1)х(second.length()+1),
+     *      строка длиной min(first.length(), second.length())
      */
     public static String longestCommonSubSequence(String first, String second) {
-        throw new NotImplementedError();
+        int m = first.length() + 1;
+        int n = second.length() + 1;
+        int[][] lcs = new int[n][m];
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (first.charAt(j - 1) == second.charAt(i - 1))
+                    lcs[i][j] = lcs[i - 1][j - 1] + 1;
+                else lcs[i][j] = Math.max(lcs[i - 1][j], lcs[i][j - 1]);
+            }
+        }
+        Stack<Character> stack = new Stack<>();
+        StringBuilder result = new StringBuilder();
+        int i = second.length();
+        int j = first.length();
+        while (i != 0 && j != 0) {
+            if (lcs[i - 1][j - 1] == lcs[i][j] - 1 && first.charAt(j - 1) == second.charAt(i - 1)) {
+                stack.push(first.charAt(j - 1));
+                i--;
+                j--;
+            } else if (lcs[i - 1][j] == lcs[i][j]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+        while (!stack.isEmpty()) {
+            result.append(stack.pop());
+        }
+        return result.toString();
     }
-
     /**
      * Наибольшая возрастающая подпоследовательность
      * Сложная
@@ -33,9 +68,48 @@ public class JavaDynamicTasks {
      * Если самых длинных возрастающих подпоследовательностей несколько (как в примере),
      * то вернуть ту, в которой числа расположены раньше (приоритет имеют первые числа).
      * В примере ответами являются 2, 8, 9, 12 или 2, 5, 9, 12 -- выбираем первую из них.
+     *
+     * Трудоемкость T = O(n^2) - вложенный цикл с n^2 операцией
+     * Ресурсоемкость R = O(n) - два массива размером n, лист размером n и строка длиной n (в худшем случае)
      */
     public static List<Integer> longestIncreasingSubSequence(List<Integer> list) {
-        throw new NotImplementedError();
+        List<Integer> result = new ArrayList<>();
+        if (list.isEmpty()) return result;
+        int[] arrayOfLength = new int[list.size()];
+        int[] arrayOfIndex = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            arrayOfLength[i] = 1;
+            arrayOfIndex[i] = -1;
+            int maxLength = 0;
+            for (int j = 0; j < i; j++) {
+                if (list.get(j) < list.get(i)) {
+                    if (1 + arrayOfLength[j] > arrayOfLength[j] && 1 + arrayOfLength[j] > maxLength) {
+                        arrayOfLength[i] = 1 + arrayOfLength[j];
+                        maxLength = 1 + arrayOfLength[j];
+                        arrayOfIndex[i] = j;
+                    }
+                }
+            }
+        }
+        int answer = arrayOfLength[0];
+        int position = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if (arrayOfLength[i] > answer) {
+                answer = arrayOfLength[i];
+                position = i;
+            }
+        }
+        while (position != -1) {
+            for (int i = position; i > 0; i--) {
+                if (arrayOfIndex[i - 1] == arrayOfIndex[i] && list.get(i) > list.get(i - 1)) {
+                    position--;
+                } else break;
+            }
+            result.add(list.get(position));
+            position = arrayOfIndex[position];
+        }
+        Collections.reverse(result);
+        return result;
     }
 
     /**
--- test/lesson7/DynamicTestsJava.kt
+++ test/lesson7/DynamicTestsJava.kt
@@ -1,7 +1,9 @@
 package lesson7
 
 import org.junit.jupiter.api.Tag
-import kotlin.test.Test
+import org.junit.jupiter.api.Test
+import java.util.*
+import kotlin.test.*
 
 class DynamicTestsJava : AbstractDynamicTests() {
 
@@ -9,12 +11,20 @@ class DynamicTestsJava : AbstractDynamicTests() {
     @Tag("5")
     fun testLongestCommonSubSequenceJava() {
         longestCommonSubSequence { first, second -> JavaDynamicTasks.longestCommonSubSequence(first, second) }
+        // My Test
+        assertEquals("", JavaDynamicTasks.longestCommonSubSequence("", "1111"))
+        assertEquals("", JavaDynamicTasks.longestCommonSubSequence("", ""))
     }
 
     @Test
     @Tag("7")
     fun testLongestIncreasingSubSequenceJava() {
         longestIncreasingSubSequence { JavaDynamicTasks.longestIncreasingSubSequence(it) }
+        // My Test
+        assertEquals(listOf(1), JavaDynamicTasks.longestIncreasingSubSequence(listOf(1, 1, 1, 1, 1, 1, 1, 1)))
+        assertEquals(listOf(10), JavaDynamicTasks.longestIncreasingSubSequence(listOf(10, 9, 8, 7, 6, 5, 4, 3, 2, 1)))
+        assertEquals(listOf(13, 14, 15, 16, 17, 18, 19),
+            JavaDynamicTasks.longestIncreasingSubSequence(listOf(13, 14, 15, 1, 2, 8, 16, 17, 18, 19)))
     }
 
     @Test
