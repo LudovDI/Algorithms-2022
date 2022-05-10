a25dc51a4628f7f8cd3e4d7ccedeefdaaef57e99
LudovDI <dima-l-01@mail.ru>
2022-05-10 21:25:31 +0300

    Урок 6

--- src/lesson6/JavaGraphTasks.java
+++ src/lesson6/JavaGraphTasks.java
@@ -1,9 +1,9 @@
 package lesson6;
 
 import kotlin.NotImplementedError;
+import lesson6.impl.GraphBuilder;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @SuppressWarnings("unused")
 public class JavaGraphTasks {
@@ -32,11 +32,32 @@ public class JavaGraphTasks {
      *
      * Справка: Эйлеров цикл -- это цикл, проходящий через все рёбра
      * связного графа ровно по одному разу
+     *
+     * Трудоемкость T = O(E^2) - метод eulerCycle вызывается Е раз, трудоемкость внутри цикла for O(Е),
+     *                           где E - количество дуг в графе.
+     * Ресурсоемкость R = O(Е) - 2 списка размером Е.
      */
     public static List<Graph.Edge> findEulerLoop(Graph graph) {
-        throw new NotImplementedError();
+        List<Graph.Edge> result = new LinkedList<>();
+        Graph.Vertex randomVertex = new GraphBuilder.VertexImpl("");
+        if (graph.getVertices().size() <= 1) return result;
+        for (Graph.Vertex vertex : graph.getVertices()) {
+            if (graph.getNeighbors(vertex).size() % 2 != 0) return result;
+            randomVertex = vertex;
+        }
+        List<Graph.Edge> path = new LinkedList<>();
+        eulerCycle(graph, randomVertex, path, result);
+        return result.size() == graph.getEdges().size() ? result : Collections.emptyList();
     }
-
+    static void eulerCycle(Graph graph, Graph.Vertex v, List<Graph.Edge> path, List<Graph.Edge> result) {
+        for (Graph.Vertex vertex : graph.getNeighbors(v)) {
+            if (!path.contains(graph.getConnection(v, vertex))) {
+                path.add(graph.getConnection(v, vertex));
+                eulerCycle(graph, vertex, path, result);
+                result.add(graph.getConnection(v, vertex));
+            }
+        }
+    }
     /**
      * Минимальное остовное дерево.
      * Средняя
@@ -64,9 +85,33 @@ public class JavaGraphTasks {
      * E    F    I
      * |
      * J ------------ K
+     *
+     * Трудоемкость T = O(V^2*E) - цикл while с V операцией, вложенный цикл for с Е операцией,
+     *      трудоемкость внутри цикла for O(V) (где V - количество вершин, E - количество дуг в графе)
+     * Ресурсоемкость R = O(V) - очередь размером V, список размером V, Graph размером V.
      */
     public static Graph minimumSpanningTree(Graph graph) {
-        throw new NotImplementedError();
+        Queue<Graph.Vertex> queueVertex = new ArrayDeque<>(graph.getVertices());
+        List<String> treeVertices = new LinkedList<>();
+        GraphBuilder graphBuilder = new GraphBuilder();
+        while (!queueVertex.isEmpty()) {
+            Graph.Vertex vertex = queueVertex.poll();
+            for (Graph.Vertex neighbor : graph.getNeighbors(vertex)) {
+                if (!treeVertices.contains(vertex.getName()) || !treeVertices.contains(neighbor.getName())) {
+                    if (!treeVertices.contains(vertex.getName())) {
+                        treeVertices.add(vertex.getName());
+                        graphBuilder.addVertex(vertex.getName());
+                    }
+                    if (!treeVertices.contains(neighbor.getName())) {
+                        treeVertices.add(neighbor.getName());
+                        graphBuilder.addVertex(neighbor.getName());
+                    }
+                    graphBuilder.addConnection(vertex, neighbor, 1);
+
+                }
+            }
+        }
+        return graphBuilder.build();
     }
 
     /**
--- test/lesson6/GraphTestsJava.kt
+++ test/lesson6/GraphTestsJava.kt
@@ -1,19 +1,60 @@
 package lesson6
 
+import lesson6.impl.GraphBuilder
 import org.junit.jupiter.api.Tag
-import kotlin.test.Test
+import org.junit.jupiter.api.Test
+import java.util.*
+import kotlin.test.*
 
 class GraphTestsJava : AbstractGraphTests() {
     @Test
     @Tag("6")
     fun testFindEulerLoopJava() {
         findEulerLoop { let { JavaGraphTasks.findEulerLoop(it) } }
+        //My Test
+        val graph1 = GraphBuilder().apply {
+            val a = addVertex("A")
+        }.build()
+        val loop1 = JavaGraphTasks.findEulerLoop(graph1)
+        assertEquals(0, loop1.size)
+        val graph2 = GraphBuilder().apply {
+            val a = addVertex("A")
+            val b = addVertex("B")
+            addConnection(a, b)
+        }.build()
+        val loop2 = JavaGraphTasks.findEulerLoop(graph2)
+        assertEquals(0, loop2.size)
+        val graph3 = GraphBuilder().apply {
+            val a = addVertex("A")
+            val b = addVertex("B")
+            val c = addVertex("C")
+            addConnection(a, b)
+            addConnection(a, c)
+            addConnection(b, c)
+        }.build()
+        val loop3 = JavaGraphTasks.findEulerLoop(graph3)
+        assertEquals(3, loop3.size)
     }
 
     @Test
     @Tag("7")
     fun testMinimumSpanningTreeJava() {
         minimumSpanningTree { let { JavaGraphTasks.minimumSpanningTree(it) } }
+        // My Test
+        val graph = GraphBuilder().apply {
+            val a = addVertex("A")
+            val b = addVertex("B")
+            val c = addVertex("C")
+            val d = addVertex("D")
+            addConnection(a, b)
+            addConnection(a, c)
+            addConnection(a, d)
+            addConnection(b, c)
+            addConnection(b, d)
+            addConnection(c, d)
+        }.build()
+        val tree = JavaGraphTasks.minimumSpanningTree(graph)
+        assertEquals(3, tree.edges.size)
     }
 
     @Test
